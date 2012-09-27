40c9c0c2a24bf44aee5f71937699c870f61c6204
Lyndon Maydwell <maydwell@gmail.com>
2012-09-27 18:43:13 +0800

    Removing bogus stats

--- client/stats.js
+++ client/stats.js
@@ -4,8 +4,6 @@ Template.stats.helpers({ minx : bound(_.min, 'x')
                        , miny : bound(_.min, 'y')
                        , maxx : bound(_.max, 'x')
                        , maxy : bound(_.max, 'y')
-                       , dx   : (maxx - minx)
-                       , dy   : (maxy - miny)
                       })
 
 function bound(f,i){
